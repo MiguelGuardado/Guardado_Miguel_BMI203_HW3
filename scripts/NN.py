746550950b21ec6a4dfbc49af3e8ca99c2ad13ac
laurashub <shublaura@gmail.com>
2021-02-22 11:49:35 -0800

    small changes

--- scripts/NN.py
+++ scripts/NN.py
@@ -1,6 +1,7 @@
 import numpy as np
 class NeuralNetwork:
     def __init__(self, setup=[[68,25,"sigmoid",0],[25,1,"sigmoid",0]],lr=.05,seed=1,error_rate=0,bias=1,iter=500,lamba=.00001,simple=0):
+        #Note - these paramaters are examples, not the required init function parameters
 
     def make_weights(self):
 
@@ -8,7 +9,7 @@ class NeuralNetwork:
 
     def backprop(self):
 
-    def fit(self:
+    def fit(self):
 
     def predict(self):
 
