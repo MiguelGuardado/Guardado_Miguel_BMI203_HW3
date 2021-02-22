fa9234ccd4a2c973375b4b0acea02a525db32d18
laurashub <shublaura@gmail.com>
2021-02-22 12:04:38 -0800

    pass

--- scripts/NN.py
+++ scripts/NN.py
@@ -2,17 +2,22 @@ import numpy as np
 class NeuralNetwork:
     def __init__(self, setup=[[68,25,"sigmoid",0],[25,1,"sigmoid",0]],lr=.05,seed=1,error_rate=0,bias=1,iter=500,lamba=.00001,simple=0):
         #Note - these paramaters are examples, not the required init function parameters
+        pass
 
     def make_weights(self):
+        pass
 
     def feedforward(self):
+        pass
 
     def backprop(self):
+        pass
 
     def fit(self):
+        pass
 
     def predict(self):
-
+        pass
 
 def activation(x):
-    
+    pass 
